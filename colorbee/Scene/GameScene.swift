6e4af803447cae8a339e8347b0a6576e312bd56d
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-06-08 15:23:03 +0200

    nettoyage code

---
+++ colorbee/Scene/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  colorbee
+//
+//  Created by Snoopy on 08/06/2018.
+//  Copyright © 2018 EMANET. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
